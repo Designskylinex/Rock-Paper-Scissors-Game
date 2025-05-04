419f844575471c16f5a7b227f4ccedfc862b7600
Designskylinex <204343691+Designskylinex@users.noreply.github.com>
2025-05-03 20:16:14 -0400

    Made code more eye pleasing, added an infinite calling to getHumanChoice by accident.

--- game.js
+++ game.js
@@ -2,27 +2,24 @@ console.log("Welcome to rock paper scissors")
 
 // Takes user input to determine if they chose a correct answer
 function getHumanChoice() {
-    let userChoice = prompt("Rock Paper or Scissors?")
+    let userChoice = prompt("Rock Paper or Scissors?");
     return userChoice;
 }
 
 // Function that chooses a random number for the computer
 function randomChoice(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Stores the random choice in a constant variable called choice
-const choice = randomChoice(1, 3);
 
 // Function for computer with the choice
 function getComputerChoice(choice) {
+    const num = randomChoice(1, 3)
     if (choice == 3) {
         return "Scissors";
-    }
-    else if (choice == 2) {
+    } else if (choice == 2) {
         return "Paper";
-    }
-    else if (choice == 1) {
+    } else if (choice == 1) {
         return "Rock";
     }
 }
@@ -35,22 +32,26 @@ let computerScore = 0
 
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice == "Paper" && computerChoice == "Scissors") {
-        console.log("Computer Wins!")
-    } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
-        console.log("You win!")
-    } else if (humanChoice == "Rock" && computerChoice == "Paper") {
-        console.log("Computer Wins!")
-    } else if (humanChoice == "Paper" && computerChoice == "Rock") {
-        console.log("You Win!")
-    } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
-        console.log("Computer wins!")
-    } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
-        console.log("You Win!")
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if (
+        (humanChoice === "Paper" && computerChoice === "Scissors") ||
+        (humanChoice === "Rock" && computerChoice === "Paper") ||
+        (humanChoice === "Scissors" && computerChoice === "Rock")
+    ) {
+        console.log("Computer wins!");
+        computerScore += 1
+    } else {
+        console.log("You win!");
+        humanScore += 1
     }
 }
 
-
 // Initiates a round
-getHumanChoice();
-playRound(getHumanChoice, getComputerChoice);
+const humanChoice = getHumanChoice()
+const computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
